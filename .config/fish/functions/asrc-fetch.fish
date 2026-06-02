46ae2223297b265e1f7fcbe2a518855f18f02195
Simon Hulse <simonhulse@protonmail.com>
2026-06-02 12:22:56 -0400

    Fix asrc functions: rename exxact to daisy, fix variable indirection case, add missing return 1

--- .config/fish/functions/asrc-fetch.fish
+++ .config/fish/functions/asrc-fetch.fish
@@ -1,16 +1,18 @@
 function asrc-fetch
-    set signature "asrc-fetch (exxact|orin) src [dst]"
+    set signature "asrc-fetch (daisy|orin) src [dst]"
     if test (count $argv) -lt 2 -o (count $argv) -gt 3
         echo $signature
         echo "2 or 3 arguments are required"
         return 1
     end
 
-    if not contains $argv[1] exxact orin
+    if not contains $argv[1] daisy orin
         echo $signature
-        echo "First argument must be `exxact` or `orin`."
+        echo "First argument must be `daisy` or `orin`."
+        return 1
     else
-        set host $$argv[1]
+        set varname (string upper $argv[1])
+        set host $$varname
     end
 
     if test (count $argv) -eq 2
