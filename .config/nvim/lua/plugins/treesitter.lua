65c183dabd89792e5232afdee0faa2b2c2dfaaaf
Simon Hulse <simonhulse@protonmail.com>
2026-06-05 12:56:21 -0400

    nvim: add markdown treesitter parsers

--- .config/nvim/lua/plugins/treesitter.lua
+++ .config/nvim/lua/plugins/treesitter.lua
@@ -5,7 +5,7 @@ return {
   config = function()
     require("nvim-treesitter.install").compilers = { "gcc" }
     require("nvim-treesitter").install({
-      "bash", "fish", "java", "latex", "lua", "make", "python", "rust", "vim",
+      "bash", "fish", "java", "latex", "lua", "make", "markdown", "markdown_inline", "python", "rust", "vim",
     })
     vim.api.nvim_create_autocmd("FileType", {
       callback = function() pcall(vim.treesitter.start) end,
